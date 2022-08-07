992fe8f0500423fddc694fb4f8bab184a16a31ce
zhy2on <dhwlgus5940@gmail.com>
2022-08-07 21:34:32 +0900

    Fix: cone theta도 parsing할 때 범위 체크할 수 있도록 추가

--- srcs/parse/parse_figures_utils.c
+++ srcs/parse/parse_figures_utils.c
@@ -6,7 +6,7 @@
 /*   By: jihoh <jihoh@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/07 17:00:28 by jihoh             #+#    #+#             */
-/*   Updated: 2022/08/07 17:14:37 by jihoh            ###   ########.fr       */
+/*   Updated: 2022/08/07 21:33:40 by jihoh            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,6 +20,24 @@ t_figures	*get_figures_node(int type)
 	figures->type = type;
 	figures->next = NULL;
 	return (figures);
+}
+
+void	check_figures_setting2(t_figures elem)
+{
+	if (elem.type == CY)
+	{
+		if (elem.fig.cy.height < 0 || elem.fig.cy.height > INFINITY)
+			put_error("cylinder height is out of range");
+		if (elem.fig.cy.r < 0 || elem.fig.cy.r > INFINITY)
+			put_error("cylinder diameter is out of range");
+	}
+	if (elem.type == CON)
+	{
+		if (elem.fig.con.height < 0 || elem.fig.con.height > INFINITY)
+			put_error("cone height is out of range");
+		if (elem.fig.con.theta < 0 || elem.fig.con.theta >= 180)
+			put_error("cone theta is out of range");
+	}
 }
 
 void	check_figures_setting(t_figures elem)
@@ -37,18 +55,7 @@ void	check_figures_setting(t_figures elem)
 		if (elem.fig.sp.r < 0 || elem.fig.sp.r > INFINITY)
 			put_error("sphere diameter is out of range");
 	}
-	if (elem.type == CY)
-	{
-		if (elem.fig.cy.height < 0 || elem.fig.cy.height > INFINITY)
-			put_error("cylinder height is out of range");
-		if (elem.fig.cy.r < 0 || elem.fig.cy.r > INFINITY)
-			put_error("cylinder diameter is out of range");
-	}
-	if (elem.type == CON)
-	{
-		if (elem.fig.con.height < 0 || elem.fig.con.height > INFINITY)
-			put_error("cone height is out of range");
-	}
+	check_figures_setting2(elem);
 }
 
 void	push_figures_node(t_scene *scene, t_figures *new)
